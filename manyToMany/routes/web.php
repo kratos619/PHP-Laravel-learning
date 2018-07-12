1f14360e275ddecb99929edada995ae5e21b0f46
kratos619 <gaurav.palaspagar@hotmail.com>
2018-07-12 11:35:39 +0530

    start new lerning Polymorphic

--- manyToMany/routes/web.php
+++ manyToMany/routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\User;
+use App\Role;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +14,38 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+//create data base
+Route::get('/insert', function () {
+    $user = User::find(1);
+    $role = new Role(['name'=>' Administrator']);
+    $user->role()->save($role);
+    return view('welcome');
+});
+//Read data base
+Route::get('/read', function () {
+    $user = User::findOrFail(1);
+//dd($user);
+    foreach($user->role as $role){
+        echo $role->name;
+    }
+});
+//Update data base
+Route::get('/update', function () {
+    $user = User::findOrFail(1);
+if($user-> has('role')){
+    foreach($user->role as $role){
+            $role->name = 'admin';
+            $role->save();
+    }
+}
+});
+//Delete data base
+Route::get('/delete', function () {
+    $user = User::find(1);
+
+    foreach($user->role as $role){
+            $role->whereId(1)->delete();
+    }
+
 });
