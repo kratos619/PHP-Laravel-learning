e1bab6b73b4dffdbf5b3f05467551164de171f19
kratos619 <gaurav.palaspagar@hotmail.com>
2018-07-12 08:18:57 +0530

    CRUD in ONE TO Many Reletionship

--- oneToMany/routes/web.php
+++ oneToMany/routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\User;
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +13,42 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+echo "<h2>hi</h2>";
+//return view('welcome');
+});
+Route::get('/hi', function () {
+echo "<h2>hi2</h2>";
+//return view('welcome');
+});
+
+// insert Data
+Route::get('/create', function(){
+  $user = User::findOrFail(1);
+  $post = new Post(['title'=>'first post title','body'=>'this is content one']);
+if($user->posts()->save($post)){
+echo "data insert Successfull";
+}
+});
+
+// read data
+Route::get('/users', function () {
+    $user = User::findOrFail(1);
+    return $user->posts;
+});
+
+// update data
+Route::get('/update', function(){ 
+  $user = User::find(1);
+  $post = $user->posts()->whereId(1)->update(['title'=>'update first post title','body'=>'update this is content one']);
+if($post){
+echo "data update Successfull";
+}
+});
+// delete data
+Route::get('/update', function(){ 
+  $user = User::find(1);
+  $post = $user->posts()->whereId(1)->delete();
+if($post){
+echo "data delete Successfull";
+}
 });
